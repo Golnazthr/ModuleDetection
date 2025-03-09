dce285abf32b45fca0b252f022dfab1a51957e79
amirho3in <amirhoseinhaerian@gmail.com>
2025-03-09 10:36:44 +0100

    feat: make the file pathes to data dynamic

--- genepioneer/evaluation.py
+++ genepioneer/evaluation.py
@@ -18,14 +18,19 @@ from scipy.stats import mannwhitneyu
 from genepioneer import DataLoader
 
 class Evaluation:
-    def __init__(self):
+    def __init__(self, data_path, cancer_gene_path=None, module_data_path=None, benchmark_data_path=None):
         self.gp = GProfiler(return_dataframe=True)
         
-        self.benchmark_genes = self.read_benchmark_genes("../Data/benchmark-data")
-        self.network_genes = self.read_network_genes("../Data/cancer-gene-data")
-        self.modules = self.read_modules("../Data/module-data")
+        self.cancer_gene_path = cancer_gene_path or "../Data/cancer-gene-data"
+        self.module_data_path = module_data_path or "../Data/module-data"
+        self.benchmark_data_path = benchmark_data_path or "../Data/benchmark-data"
+
+        self.benchmark_genes = self.read_benchmark_genes(self.benchmark_data_path)
+        self.network_genes = self.read_network_genes(self.cancer_gene_path)
+        self.modules = self.read_modules(self.module_data_path)
         self.result = self.eval(self.network_genes, self.benchmark_genes)
         self.module_results = self.evaluate_modules(self.modules)
+        self.data_path = data_path
     
     def read_modules(self, benchmark_folder):
         benchmark_genes = {}
@@ -59,7 +64,7 @@ class Evaluation:
         for filepath in glob.glob(os.path.join(network_folder, '*.csv')):
             cancer_type = os.path.basename(filepath).split('.')[0]
             cancer_type = cancer_type.replace('_network_features', '')
-            data_loader = DataLoader(cancer_type)
+            data_loader = DataLoader(cancer_type, self.data_path)
             genes_with_cases, cases_with_genes, total_cases = data_loader.load_TCGA()
             genes = list(genes_with_cases.keys())
             network_genes[cancer_type] = genes
